2df17425e771307b21760f22cf601e7969afd241
Psyballa <justin.telmo@gmail.com>
2014-08-31 18:09:29 -0700

    Making a Telmo Test

---
+++ telmoTutorial/init.lua
@@ -0,0 +1,5 @@
+minetest.register_node("tutorial:decowood", {
+	description = {"Deadwood"},
+	tiles = {"tutorial_decowood.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+})
