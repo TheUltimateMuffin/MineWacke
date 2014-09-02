aa422aea580705b52be8ae57ddf1d8e4c208f957
Psyballa <psyballa@gmail.com>
2014-09-02 02:05:29 -0700

    Adding a function to return the players' name from a blackjack dealer.

--- mods/blackjack/blackjackDealer.lua
+++ mods/blackjack/blackjackDealer.lua
@@ -5,5 +5,11 @@ blackjackDealer.__index = blackjackDealer
 
 function blackjackDealer.new()
 	local self = setmetatable({}, blackjackDealer)
-	self.players = blackjackDealer.getPlayers()
+	self.player = blackjackDealer.getPlayer()
+end
+
+function blackjackDealer.getPlayer() 
+	if player:is_player() then
+		return player:get_player_name()
+	end
 end
